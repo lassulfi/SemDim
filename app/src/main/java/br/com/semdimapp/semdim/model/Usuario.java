98fc3bf0a570fab919f7d4c998c06c28e35c03d5
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-08-17 22:09:49 -0300

    Criada classe Usuario

---
+++ app/src/main/java/br/com/semdimapp/semdim/model/Usuario.java
@@ -0,0 +1,54 @@
+package br.com.semdimapp.semdim.model;
+
+/**
+ * Classe usuario.
+ */
+public class Usuario {
+
+    //Atributos
+    private String id; //ID do usuario
+    private String nome; //username do usuario
+    private String email; //email
+    private String senha; //senha
+
+    //Construtor
+    public Usuario( String nome, String email, String senha){
+        this.id = null;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    //Getters & Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
