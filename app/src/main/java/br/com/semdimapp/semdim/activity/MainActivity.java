1cae52fcf276256b6f3e7514e57bda97a844bf29
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-08-20 22:52:43 -0300

    Inicio da configuração da MainActivity

--- app/src/main/java/br/com/semdimapp/semdim/activity/MainActivity.java
+++ app/src/main/java/br/com/semdimapp/semdim/activity/MainActivity.java
@@ -1,15 +1,38 @@
 package br.com.semdimapp.semdim.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import br.com.semdimapp.semdim.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Atributos
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Recupera os elementos da tela
+
+        //Configuração da toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar_principal);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
+        //Configuração do viewpager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Configuração do tabLayout
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
