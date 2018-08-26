ab295d7cf557b2195895f7d119242d2594d8eb5d
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-08-26 16:28:47 -0300

    Atualizado método de Login

--- app/src/main/java/br/com/semdimapp/semdim/controller/LoginController.java
+++ app/src/main/java/br/com/semdimapp/semdim/controller/LoginController.java
@@ -1,20 +1,26 @@
 package br.com.semdimapp.semdim.controller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
+import br.com.semdimapp.semdim.activity.LoginActivity;
+import br.com.semdimapp.semdim.activity.MainActivity;
 import br.com.semdimapp.semdim.config.FirebaseConfig;
 import br.com.semdimapp.semdim.helper.Base64Custom;
 import br.com.semdimapp.semdim.helper.Preferences;
+import br.com.semdimapp.semdim.model.Contato;
 import br.com.semdimapp.semdim.model.Usuario;
 
 /**
@@ -23,6 +29,8 @@ import br.com.semdimapp.semdim.model.Usuario;
 public class LoginController {
 
     //Atributos
+    private static final String DATABASEERROR = "databaseError:Error";
+
     private DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
 
@@ -42,7 +50,7 @@ public class LoginController {
         this.usuarioController = new UsuarioController();
     }
 
-    public boolean verificarUsuarioLogado(){
+    public boolean verificarUsuarioLogado(Context context){
 
         if(firebaseAuth.getCurrentUser() != null){
             return true;
@@ -51,14 +59,17 @@ public class LoginController {
         return false;
     }
 
+    @Deprecated
     public void logarUsuario(final String email, String password, final Context context){
+
+        firebaseAuth = FirebaseConfig.getFirebaseAuth();
 
         firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 //Verifica se o processo de login foi bem sucedido
-                if(task.isSuccessful()){
+                if(task.isSuccessful()) {
 
                     loggedUserId = Base64Custom.encodeBase64(email);
 
@@ -82,18 +93,18 @@ public class LoginController {
 
                         @Override
                         public void onCancelled(DatabaseError databaseError) {
-
+                            Log.w(DATABASEERROR, databaseError.getMessage());
                         }
                     };
-
+                    databaseReference.addListenerForSingleValueEvent(userValueEventListener);
                     success = true;
                 }
             }
         });
-
     }
 
     public boolean isSuccess(){
+
         return success;
     }
 
